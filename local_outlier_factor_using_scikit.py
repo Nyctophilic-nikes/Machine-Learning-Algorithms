ce5f130d245820b9485c6ee085288c666a31b3c0
alien-space-character <107383684+Nyctophilic-nikes@users.noreply.github.com>
2023-03-28 17:55:17 +0530

    Create local_outlier_factor_using_scikit.py
    Local Outlier Factor implementation using scikit

---
+++ local_outlier_factor_using_scikit.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from scipy.io import arff
+data = arff.loadarff("C:\\Users\\HP\\SML\\Sample data\\ALOI.arff")
+
+#Load data
+df = pd.DataFrame(data[0])
+
+df_new = df.drop(columns=["outlier", "id"])
+
+# initialize the LOF model
+lof = LocalOutlierFactor(n_neighbors=20, contamination=0.1)
+
+# fit the model and make predictions
+y_pred = lof.fit_predict(df_new)
+
+# add the predictions to the DataFrame
+df_new["lof"] = y_pred
+
+# display the DataFrame
+display(df_new)
